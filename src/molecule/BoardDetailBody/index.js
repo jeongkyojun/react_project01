e8e0a845c37c1522c0f040ae5e8b00035e48aeaf
jeongkyojun <kyojun2002@naver.com>
2024-01-09 19:16:33 +0900

    게시판 마크다운 적용중

--- src/molecule/BoardDetailBody/index.js
+++ src/molecule/BoardDetailBody/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { StyledBoardDetailBody } from "./style";
+import ReactMarkdown from "react-markdown";
 import MDEditor from "@uiw/react-md-editor";
 
-const BoardDetailBody = ({texts})=>{
+const BoardDetailBody = ({texts,onChangeHandler})=>{
     return(
         <StyledBoardDetailBody>
+            <MDEditor
+                value={texts}
+                onChange={onChangeHandler}>
+
+            </MDEditor>
+            <MDEditor.Markdown
+                width={'100%'}
+                height={'100%'}
+                source={texts}
+            ></MDEditor.Markdown>
         </StyledBoardDetailBody>
     );
 }
--- src/page/BoardDetailPage/index.js
+++ src/page/BoardDetailPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledBoardDetailBody, StyledBoardDetailFooter, StyledBoardDetailHead, StyledBoardDetailPage } from "./style";
 import Navbar from "../../organ/Navbar";
 import { useLocation } from "react-router-dom";
@@ -7,6 +7,19 @@ import BoardDetailBody from "../../molecule/BoardDetailBody";
 
 const BoardDetailPage = ()=>{
     const {state} = useLocation();
+
+    const [mdText,setMdText] = useState(
+        `## 시작 
+         **공지사항** 
+            1. 바르고 고운말을 사용합니다. 
+            2. 비난과 비방을 목적으로 한 글을 작성하지 맙시다. 
+            3. 무분별한 도배는 자제합시다.
+        `
+    );
+
+    const onChangeHandler = (e)=>{
+        setMdText(e);
+    }
 
     return(
         <StyledBoardDetailPage>
@@ -19,7 +32,7 @@ const BoardDetailPage = ()=>{
                 <BoardDetailHeader title={state.title} creator={state.creator} good={state.good} view={state.view} comment={state.comment} createDate={state.createDate.toLocaleString('ko-KR')}>
 
                 </BoardDetailHeader>
-                <BoardDetailBody texts={'**공지사항** <br> 안녕하세요, 관리자입니다. <br> 내용을 잘 지켜주시기 바라겠습니다.'}></BoardDetailBody>
+                <BoardDetailBody texts={mdText} onChangeHandler={onChangeHandler}></BoardDetailBody>
             </StyledBoardDetailBody>
             <StyledBoardDetailFooter>
                 정보
